8e9d8af07be73e1587de1dc0049796203d14379d
oleg-masterschool <87778112+oleg-masterschool@users.noreply.github.com>
2021-10-07 18:53:54 +0300

    Revert "OOP 3"

--- OOP3/src/pages/index.js
+++
@@ -1,8 +0,0 @@
-import { Garage } from "../components/Garage.js";
-import { vehicles } from "../data/index.js";
-
-const garageSelector = '.garage';
-
-const vehiclesInventory = new Garage({ data: vehicles }, garageSelector);
-
-vehiclesInventory.renderItems();
