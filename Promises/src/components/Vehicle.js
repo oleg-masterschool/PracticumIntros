e898e62425c76ae1e17c20f427e83eb520442a59
oleg-masterschool <87778112+oleg-masterschool@users.noreply.github.com>
2021-10-09 23:39:23 +0300

    promises and callback

--- Promises/src/components/Vehicle.js
+++ Promises/src/components/Vehicle.js
@@ -9,16 +9,6 @@ export class Vehicle {
         this._selector = selector;
         this._element;
         this._handleClick = this._handleImageClick.bind(this);
-        this._handleKeys = this._handleKeyDown.bind(this);
-        this.arrowHandler = this._arrowHandler.bind(this);
-        this._xCounter = 0;
-        this._yCounter = 0;
-        this._movement = {
-            'left': -10,
-            'right': +  10,
-            'up': -10,
-            'down': +10
-        };
     }
 
     _getTemplate() {
@@ -29,44 +19,9 @@ export class Vehicle {
             .cloneNode(true);
     }
 
-    _arrowHandler(direction) {
-        if (direction === 'left' || direction === 'right') {
-            this._xCounter = this._xCounter + this._movement[direction];
-        }
-
-        if (direction === 'up' || direction === 'down') {
-            this._yCounter = this._yCounter + this._movement[direction];
-        }
-        this._element.style.transform = `translate(${this._xCounter}px, ${this._yCounter}px)`;
-    }
-
-    _handleKeyDown(e) {
-        e.preventDefault();
-        const { key } = e;
-        const directions = {
-            "ArrowLeft": 'left',
-            "ArrowRight": 'right',
-            "ArrowUp": 'up',
-            "ArrowDown": 'down',
-        };
-
-        if (key in directions) {
-            this.arrowHandler(directions[key]);
-        }
-    }
-
     _handleImageClick() {
         this._element
             .classList.toggle(vehicleActiveClass);
-
-        if (this._element.classList.contains(vehicleActiveClass)) {
-            document.addEventListener('keydown', this._handleKeys);
-        } else {
-            this._element.style = null;
-            this._xCounter = 0;
-            this._yCounter = 0;
-            document.removeEventListener('keydown', this._handleKeys);
-        }
     }
 
 
