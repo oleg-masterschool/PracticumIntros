e99991a74db6c7d41bb4760c56fe92833067f6c4
oleg-masterschool <87778112+oleg-masterschool@users.noreply.github.com>
2021-10-10 22:33:36 +0300

    call backs and promises

--- Promises/src/components/Vehicle.js
+++ Promises/src/components/Vehicle.js
@@ -1,5 +1,3 @@
-import { waterVehicleSelector } from "./WaterVehicle.js";
-
 const vehicleSelector = '.vehicle';
 export const vehicleImageSelector = '.vehicle__image';
 const vehicleActiveClass = 'vehicle_is-active';
@@ -9,6 +7,7 @@ export class Vehicle {
         this._selector = selector;
         this._element;
         this._handleClick = this._handleImageClick.bind(this);
+        this.hollerInterval;
     }
 
     _getTemplate() {
@@ -19,9 +18,27 @@ export class Vehicle {
             .cloneNode(true);
     }
 
+    _hollerCallback() {
+        const hollersElement = this._element.querySelector('.vehicle__hollers');
+        const holler = document
+            .querySelector('.holler')
+            .content
+            .querySelector('.holler__item')
+            .cloneNode(true);
+
+        hollersElement.append(holler);
+    }
+
     _handleImageClick() {
         this._element
             .classList.toggle(vehicleActiveClass);
+
+        clearInterval(this.hollerInterval);
+
+        if (this._element.classList.contains(vehicleActiveClass)
+            && this._element.classList.contains('water')) {
+            this.hollerInterval = setInterval(this._hollerCallback.bind(this), 500);
+        }
     }
 
 
