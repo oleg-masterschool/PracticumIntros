91d98b30b7885fc5a79d1c7d7dc26c51226ee407
oleg-masterschool <87778112+oleg-masterschool@users.noreply.github.com>
2021-10-11 00:50:59 +0300

    APIs

--- APIs/src/index.js
+++ APIs/src/index.js
@@ -3,3 +3,16 @@ const endpoint = 'https://jsonplaceholder.typicode.com/posts'
 fetch(endpoint)
     .then((res) => res.json())
     .then((data) => console.log(data));
+
+fetch(endpoint, {
+    method: 'POST',
+    headers: {
+        "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+        username: 'John Doe',
+        age: 22
+    })
+})
+    .then((res) => res.json())
+    .then((data) => console.log(data));
